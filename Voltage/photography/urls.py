8fd2b7a35354367cc738c85a93e6cd518e50d831
kaveeshaile <kaveeshaile2gmail.com>
2020-08-23 09:25:01 +0530

    add photo_customer main page and urls ,views files

--- Voltage/photography/urls.py
+++ Voltage/photography/urls.py
@@ -8,3 +8,10 @@ urlpatterns = [
 urlpatterns = [
     path('', views.photo_update, name='photo_update')
 ]
+
+urlpatterns = [
+    path('', views.customer_main, name='customer_main')
+]
+urlpatterns = [
+    path('', views.photo_checkdelete, name='photo_checkdelete')
+]
--- Voltage/photography/views.py
+++ Voltage/photography/views.py
@@ -7,3 +7,8 @@ def photo_add(request): return render(request, 'photo_add.html')
 
 
 def photo_update(request): return render(request, 'photo_update.html')
+def photo_checkdelete(request): return render(
+    request, 'photo_checkdelete.html')
+
+
+def customer_main(request): return render(request, 'customer_main.html')
