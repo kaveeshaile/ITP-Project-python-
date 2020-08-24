e2bcae33cd88421f3e396c1538f8acd573d321dd
UDITHA J <udithaj.98@gmail.com>
2020-08-24 12:52:35 +0530

    Database connection created

--- Voltage/Voltage/settings.py
+++ Voltage/Voltage/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'Voltage.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'voltage_entertainment',
+        'USER': 'root',
+        'PASSWORD': 'mysql',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
