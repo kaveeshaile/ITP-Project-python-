9fb50cac62e3b9a88b286e9e90b55bc53c189605
RaviBx <ravimuay@gmail.com>
2020-09-07 19:30:24 +0530

    Added css elements

--- Voltage/soundsystems/urls.py
+++ Voltage/soundsystems/urls.py
@@ -2,10 +2,16 @@ from django.urls import path
 from.import views
 
 
-urlpatterns = [
-    path('',views.sound_admin_upload,name = 'sound_admin_upload')
-]
+urlpatterns = [ 
 
-urlpatterns = [
-    path('',views.sound_main,name = 'sound_main')
+
+     path('',views.sound_main,name = 'sound_main'),
+     path('',views.sound_admin_upload,name = 'sound_admin_upload')
+  
+    
 ]
+                
+
+   
+    
+               
