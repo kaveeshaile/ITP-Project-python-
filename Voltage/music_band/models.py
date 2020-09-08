9372e2b4fc5199c4e14df0a2a64653c4c1f999bb
maduSulakshi20 <madusulakshi20@gmail.com>
2020-09-08 11:08:20 +0530

    music_band_added

--- Voltage/music_band/models.py
+++ Voltage/music_band/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class music_band_package_add(models.Model):
+ Package=models.CharField(max_length=100)
+ No of songs=models.CharField(max_length=10)
+ Charging rate=models.CharField(max_length=45)
+ Description=models.CharField(max_length=200)
+ Other Details=models.CharField(max_length=200)
+ class Meta:
+    db_table = "music_band"
+    
+class music_band_package_add(models.Model):
+ fee=models.CharField(max_length=45)
+ class Meta:
+    db_table = "child"
