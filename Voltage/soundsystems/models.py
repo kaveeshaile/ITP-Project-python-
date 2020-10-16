509b5f5edeb0ad24f4f1cda3f68ce55eff677d48
RaviBx <ravimuay@gmail.com>
2020-10-16 12:52:51 +0530

    Added sound models

--- Voltage/soundsystems/models.py
+++ Voltage/soundsystems/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+class sound_admin_upload(models.Model):
+    id = models.CharField(max_length=10, primary_key=True)     
+    Package_name=models.CharField(max_length=100)
+    Suitable_for=models.CharField(max_length=10)
+    Brand=models.CharField(max_length=45)
+    Price=models.CharField(max_length=45)
+    Description=models.CharField(max_length=2500)
+    class Meta:
+        db_table = "soundsystem"
+
+
+
+        
--- Voltage/soundsystems/views.py
+++ Voltage/soundsystems/views.py
@@ -1,15 +1,67 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib import messages
+from soundsystems.models import sound_admin_upload
+from django.shortcuts import redirect
+
+
+
+def sound_admin_upload(request): 
+    
+     if request.method == 'POST':
+             if request.POST.get('Packn') and request.POST.get('Suitable') and request.POST.get('Brand') and request.POST.get('Price'):
+                    saverecord=sound_admin_upload()
+                    saverecord.Package_name = request.POST.get('Packn')
+                    saverecord.Suitable_for = request.POST.get('Suitable')
+                    saverecord.Brand  = request.POST.get('Brand')
+                    saverecord.Price = request.POST.get('Price')
+                    saverecord.Description = request.POST.get('sounddescription')
+                    saverecord.save()
+                    return redirect(sound_update)
+     else:
+                    return render(request,'sound_admin_upload.html')
+    
+    
+
+    
+
+
+def sound_reservation(request): 
+        
+        
+        
+        
+        
+        return render(request, 'sound_reservation.html')
+
+
+ 
+def sound_userview(request): 
+        
+        if request.method == "POST":
+                if request.POST.get('id'):
+                        id = request.POST.get('id')
+                        sound = sound_admin.objects.filter(id = id)
+                        return render(request, 'sound_reservation.html',{'soundsystems':sound})
+        
+        else:
+
+                sound = sound_admin.objects.all()
+                return render(request, 'sound_userview.html',{'soundsystems':sound})
 
 
 
 
-def sound_userview(request): return render(request, 'sound_userview.html')
+def sound_update(request): 
 
-def sound_main(request): return render(request, 'sound_main.html')
 
-def sound_admin_upload(request): return render(request, 'sound_admin_upload.html')
+        sound = sound_admin.objects.all()
 
-def sound_update(request): return render(request, 'sound_update.html')
+        return render(request, 'sound_update.html',{'soundsystems':sound})
+
+
+def sound_main(request): 
+        return render(request, 'sound_main.html')
 
 
 
