f2c35802fbae973d9acecc12461c379462cdef54
RaviBx <ravimuay@gmail.com>
2020-10-21 18:26:27 +0530

    Added Sound_update and Sound_admin_control

--- Voltage/soundsystems/urls.py
+++ Voltage/soundsystems/urls.py
@@ -6,11 +6,11 @@ from.import views
 urlpatterns = [ 
 
 
-      path('sound_test',views.sound_display_admin,name = 'sound_display_admin'),
+      path('sound_update',views.sound_display_admin,name = 'sound_display_admin'),
       path('sound_userview',views.display_customers,name = 'display_customers'),
-      path('sound_test/<id>',views.edit,name = 'edit'),
-      path('sound_test2',views.sound_admin_update,name = 'sound_admin_update'),
-      path('sound_test2/<id>',views.sound_delete,name = 'sound_delete'),
+      path('sound_update/<id>',views.edit,name = 'edit'),
+      path('sound_admin_control',views.sound_admin_update,name = 'sound_admin_update'),
+      path('sound_admin_control/<id>',views.sound_delete,name = 'sound_delete'),
       path('sound_admin_upload',views.sound_insert,name = 'sound_insert'),
       
       
--- Voltage/soundsystems/views.py
+++ Voltage/soundsystems/views.py
@@ -3,6 +3,9 @@ from django.http import HttpResponse
 from django.contrib import messages
 from soundsystems.models import sound_admin
 from django.shortcuts import redirect
+import datetime
+from admin_panel.models import reservations
+from admin_panel.models import events
 
 
 
@@ -21,7 +24,7 @@ def sound_insert(request):
                 mydata.save()
                 messages.success(request, 'Package Data Saved Successfully')
 
-             return redirect(display_customers)
+                return redirect(display_customers)
                     
         else:
             #return render(request,'sound_admin_upload.html')
@@ -36,18 +39,14 @@ def sound_admin_update(request):
         if request.POST.get('id'):
          id =  request.POST.get('id')
          sound = sound_admin.objects.get(id = id)
-         print(sound)
+         #print(sound)
          sound.Package_name =  request.POST.get('packname')
          sound.Suitable_for =  request.POST.get('suitable')
          sound.Brand =  request.POST.get('brand')
          sound.Price =  request.POST.get('price')
          sound.save()
-         #return redirect(sound_display_admin)
-
-     #     return redirect('/') 
          return render(request,'sound_admin_upload.html')
-         #context = {'sound_admin': sound}
-         #return render(request,'sound_test2.html',{'soundadmin':sound})
+        
         
    else:
       
@@ -63,8 +62,9 @@ def sound_display_admin(request):
    
      sound = sound_admin.objects.all();
      #context = {'sound_admin': sound}
-     return render(request,'sound_test.html',{'soundsystem':sound})
+     #return render(request,'sound_test.html',{'soundsystem':sound})
      #return render(request,'sound_test.html', context=context)
+     return render(request,'sound_update.html',{'soundsystem':sound})
 
 
 
@@ -77,11 +77,9 @@ def edit(request,id):
      #return render(request, 'sound_test2.html', context=context)
      #return render(request,'sound_test2.html',{'soundadmin':sound})
      
-     return render(request,"sound_test2.html",{"soundsystem":sound})
+     return render(request,"sound_admin_control.html",{"soundsystem":sound})
 
     
-
-
 
 
 def display_customers(request):
@@ -101,6 +99,42 @@ def sound_delete(request, id):
 
 
 
+def bookSoundPackages(request):
+ if request.method == 'POST':
+     if request.POST.get('bookingdate') and request.POST.get('enddate'):
+          
+           eventID = request.session['eventID']
+           date = request.POST.get('bookingdate')
+           soundbook = reservations()
+           soundbook.S_Time = request.POST.get('bookingdate')
+           soundbook.E_Time = request.POST.get('enddate')
+           soundbook.Event_ID = eventID
+           soundbook.Resources_Name = 'Soundsystem'
+           sound = request.POST.get('id')
+           soundbook.Resources_ID = sound
+           mydate = date[0:10];
+           converted_date = datetime.datetime.strptime(mydate, "%Y-%m-%d").date()
+         
+           if converted_date < datetime.datetime.now().date():
+            messages.warning(request, 'Please Enter a valid Date')
+            return redirect(request.META.get('HTTP_REFERER')) 
+           else: 
+             
+               if reservations.objects.filter(S_Time__date  = converted_date, Resources_ID = sound ):
+                   
+                 messages.warning(request, 'Please check availability before make a reservation')
+                 return redirect(request.META.get('HTTP_REFERER')) 
+            
+               else:
+                 soundbook.save()
+                 return render(request, 'main_reservation_page.html')
+        
+            
+     else:
+                messages.warning(request, 'Please Fill the required Fields!')
+                return redirect(request.META.get('HTTP_REFERER'))
+ else:
+      return redirect(display_customers)
 
 
  
